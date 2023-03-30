3c98574c2683badcdff02f857f4efe585deb756f
vtomole <vtomole2@gmail.com>
2023-03-29 23:53:02 -0500

    Move pragma no cover

--- symboliq/third_party.py
+++ symboliq/third_party.py
@@ -1,5 +1,3 @@
-# pragma: no cover
-
 # Copyright (c) 2006-2023 SymPy Development Team
 #
 # All rights reserved.
@@ -28,6 +26,8 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 # DAMAGE.
+
+# pragma: no cover
 from typing import List, Union
 
 from sympy import Add, Basic, Mul, Pow
